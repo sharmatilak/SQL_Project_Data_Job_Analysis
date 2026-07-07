ae8f5fb478741e13d3282d43939dd054d1e5d746
Tilak Sharma <tilak.sharma.ai@gmail.com>
2026-07-07 17:03:35 +0530

    Solve the question about skills requiring in top-paying data analyst jobs

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -18,3 +18,13 @@ WITH top_paying_jobs AS (
     LIMIT 10
 )
 
+SELECT
+    top_paying_jobs.*,
+    skills
+
+FROM
+    top_paying_jobs
+
+INNER JOIN skills_job_dim ON skills_job_dim.job_id = top_paying_jobs.job_id
+INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
+
